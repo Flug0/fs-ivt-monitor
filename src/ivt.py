d87a55397fa887b8aced677ca632a60a252c3e5a
hugo <hugomal@kth.se>
2022-05-19 14:46:23 +0200

    started on backend class

--- src/ivt.py
+++ src/ivt.py
@@ -1,32 +1,150 @@
- # Not the best way to import... Should be:
- # from PyQt5 import QtWidgets
- # from QtWidgets import QApplication, QMainWindow
-import PyQt5.QtWidgets as QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
-
-def window():
-    app = QApplication(sys.argv)
-    win = QMainWindow()
-
-    xPos, yPos, width, height = 100, 100, 1000, 1000
-
-    win.setGeometry(xPos, yPos, width, height)
-    win.setWindowTitle("IVT Monitor")
-
-    label = QtWidgets.QLabel(win)
-    label.setText("TEST")
-    label.move(50, 50)
-
-    win.show()
-    sys.exit(app.exec_())
+import canlib
+import cantools
+from canlib import canlib
+from canlib.canlib import ChannelData, Stat
+import re
+from random import randint
 
 
+class Backend():
+    def __init__(self):
+        self.db = cantools.database.load_file('../Core/CAN/can2.dbc')
+        self.ch = self.select_channel()
+        self.data = dict()
+        self.channels = dict()
+        self.frame_ids = [self.db.get_message_by_name("IVT_Msg_Result_I").frame_id,
+                          self.db.get_message_by_name("IVT_Msg_Result_U1").frame_id,
+                          self.db.get_message_by_name("IVT_Msg_Result_U2").frame_id,
+                          self.db.get_message_by_name("IVT_Msg_Result_U3").frame_id]
 
-def main():
-    window()
+    # Get all available channels
+    def get_available_channels(self):
+        channels = dict()
+        for ch in range(canlib.getNumberOfChannels()):
+            chd = canlib.ChannelData(ch)
+            channels[ch] = chd.channel_name
+        self.channels.update(channels)
+        return channels
+
+    # Send data if connected to Virtual channel
+    def send_data(self):
+        # For every (in this case only one) frame_id representing, Current messages, send current data
+        for frame_id in self.frame_ids[:1]:
+            tx_frame = self.db.get_message_by_frame_id(frame_id)
+
+            # Create dictionary with data to be sent
+            canTx = dict()
+            canTx["IVT_Result_I_System_Error"] = 0
+            canTx["IVT_Result_I_OCS"] = 0
+            canTx["IVT_Result_I_Measurement_Error"] = 0
+            canTx["IVT_Result_I_Channel_Error"] = 0
+            canTx["IVT_ID_Result_I"] = 0
+            canTx["IVT_MsgCount_Result_I"] = 0
+            canTx["IVT_Result_I"] = 1
+            tx_data = tx_frame.encode(canTx)
+            # Send encoded data
+            self.ch.write_raw(frame_id, tx_data)
+
+        # For every frame_id representing Voltage messages, send voltage data
+        for i, frame_id in enumerate(self.frame_ids[1:]):
+            tx_frame = self.db.get_message_by_frame_id(frame_id)
+
+            # Create dictionary with data to be sent
+            canTx = dict()
+            canTx[f"IVT_Result_U{i+1}_System_Error"] = 0
+            canTx[f"IVT_Result_U{i+1}_OCS"] = 0
+            canTx[f"IVT_Result_U{i+1}_Measurement_Error"] = 0
+            canTx[f"IVT_Result_U{i+1}_Channel_Error"] = 0
+            canTx[f"IVT_ID_Result_U{i+1}"] = i+1
+            canTx[f"IVT_MsgCount_Result_U{i+1}"] = 0
+            canTx[f"IVT_Result_U{i+1}"] = 1
+
+            tx_data = tx_frame.encode(canTx)
+            # Send encoded data
+            self.ch.write_raw(frame_id, tx_data)
+        print(self.ch.readStatus())
+
+    # Get data from CAN
+    def get_data(self):
+        new_data = dict()
+        print(self.ch.readStatus())
+        while Stat.RX_PENDING in self.ch.readStatus():
+            rx = self.ch.read()
+            rx_id = rx
+            rx_data = bytes(rx.data)
+
+            try:
+                if rx_id in self.frame_ids:
+                    rx_frame = db.get_message_by_frame_id(rx_id)
+                    data = rx_frame.decode(rx.data)
+                    new_data.update(data)
+            except:
+                print("ERROR")
+        print("GOT DATA")
+        self.data.update(new_data)
+        return new_data
+
+    # Select channel by channel id, if none selected, try to connect to CAN2,
+    # if real CAN not connected, connect to VIRTUAL CAN2
+    def select_channel(self, selected = -1):
+        ch_real, ch_virtual = False, False
+        if selected == -1:
+            for ch_num in range(canlib.getNumberOfChannels()):
+                chd = canlib.ChannelData(ch_num)
+                # Looks for Virtual CAN2 channel
+                if re.search("[Vv]irtual.+(channel 1)", chd.channel_name):
+                    print(f"Found Virtual CAN channel with channel_id: {ch_num}")
+                    ch_virtual = ch_num
+                # Looks for Non-Virtual channel, with channel number 1.
+                if not re.search("[Vv]irtual", chd.channel_name) and re.match("(channel 1)", chd.channel_name):
+                    print(f"Found Non-Virtual CAN channel with channel_id: {ch_num}")
+                    ch_real = ch_num
+                    break
+        if ch_real:
+            selected = ch_real
+        elif ch_virtual:
+            selected = ch_real
+        # Open selected channel, and allow virtual channel
+        ch = canlib.openChannel(selected, canlib.canOPEN_ACCEPT_VIRTUAL)
+        ch.setBusOutputControl(canlib.canDRIVER_NORMAL)
+        # Set bitrate to 500 000
+        ch.setBusParams(canlib.canBITRATE_500K)
+        # Turn on bus
+        ch.busOn()
+        return ch
+
+    # Update data
+    def update(self):
+        pass
 
 
 
 
-main()
+connection = Backend()
+connection.get_available_channels()
+#db.select_channel()
+#db.select_channel(1)
+print(connection.frame_ids)
+print(connection.data)
+print(connection.ch.readStatus())
+connection.send_data()
+print(connection.ch.readStatus())
+connection.get_data()
+print(connection.data)
+
+
+
+
+def test():
+    db = cantools.database.load_file('../Core/CAN/can2.dbc')
+    print(db.messages)
+    print(db.get_message_by_name("IVT_Msg_Result_U1"))
+    #print(db.get_message_by_name("charger_config").signals)
+
+    num_channels = canlib.getNumberOfChannels()
+    print(f"Found {num_channels} channels")
+    for ch in range(num_channels):
+        chd = canlib.ChannelData(ch)
+        print(f"[Channel {ch}] {chd.channel_name} ({chd.card_upc_no} / {chd.card_serial_no})")
+    print(Stat.RX_PENDING)
